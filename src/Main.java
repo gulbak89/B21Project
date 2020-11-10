826834358c894a4c19167ad65c507e5f3ec2c2e5
gulbak89 <gulbakramxapkat@gmail.com>
2020-11-10 12:26:12 -0500

    added a description to the code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("World!");
+        // mac users and windows users are happy to see the commit dialog finally
     }
 }
